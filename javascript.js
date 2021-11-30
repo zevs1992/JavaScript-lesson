190870b8640c0553877c74270c18d91a16ca1e77
zevs1992 <albert.zagrutdinov@yandex.ru>
2021-11-30 13:01:21 +0300

    num

---
+++ javascript.js
@@ -0,0 +1,375 @@
+// let name = prompt('введите ваше имя')
+
+// // console.log (name.length)
+
+// console.log(name * 3.6)
+
+// let num1 = prompt('введите число')
+// let num2 = prompt('введите число')
+// let num3 = prompt('введите число')
+// let num4 = prompt('введите число')
+// let num5 = prompt('введите число')
+
+
+// функции
+
+// let arr = [1,2,3,4,5]
+
+// arr.push('2341')
+
+
+// console.log(arr)
+
+// arr.pop()
+// console.log()
+// let arr = []
+
+// arr.push('22', '33', '66')
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.unshift('167')
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// let nam1 = 10
+// let nam2 = 20
+
+// function getSum(){
+//     console.log(nam1 + nam2)
+// }
+
+// getSum()
+
+// let nam1 = 100
+// let nam2 = 50
+
+// function getSum(){
+//     console.log(nam1 - nam2)
+// }
+
+// getSum()
+
+// let name = +prompt()
+// let name2 = +prompt()
+
+
+// function getSum(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(getSum(name,name2))
+
+// let num1 = +prompt()
+// let num2 = +prompt()
+// // function getNum1(hi){
+// //     return hi.length
+// // }
+
+// const getSum = (str, str1) => str + str1
+
+// console.log(getSum(num1,num2))
+
+// let nam = +prompt()
+
+// const men = () => {
+//     if (man = 0)
+//     console.log(false)
+//     else (man > 0 )
+//     console.log(true)
+// }
+
+// let num1 = [1,2,3,4,5]
+// let num2 = [14, 10, 15]
+
+
+// const arr = (num) => {
+//     if (num1 > num1.length) 
+//     console.log('hello')
+//     else (num1 < num1,length)
+//     console.log('bay')
+    
+// }
+
+
+
+
+
+
+
+
+// условия
+
+// if(!(2==='2')){
+//     console.log(1)
+// }
+
+// if(!(2==='2')){
+//     console.log(1)
+// } else{
+//     console.log(2)
+// }
+
+// if(1>2){
+//     console.log(1)
+// } else if(2>1){
+//     console.log(2)
+// } else{
+//     console.log(3)
+// }
+
+// let num = +prompt()
+// let num2 = prompt()
+
+// if(!isNaN(num) && !isNaN(num2)){
+//     if(num > num2) {
+//         alert('больше')
+//     } else if (num < num2) {
+//         alert('меньше')
+//     } else {
+//         alert('числа равны')
+//     }
+// } else {
+//     alert('сравнеие не возможно так как это строка!')
+// }
+  
+// switch (num){
+//     case num = 1:
+//         console.log('Понидельник')
+//         break
+//     case num = 2:
+//         console.log('Вторник') 
+//         break   
+//     case num = 3:
+//         console.log('Среда')     
+//         break
+//     case num = 4:
+//             console.log('четверг')    
+//             break    
+//     case num = 5:
+//             console.log('пятница')    
+//             break
+//     case num = 6:
+//             console.log('суббота')   
+//             break
+//     case num = 7:
+//             console.log('выходной')    
+//             break
+
+//     default:
+//         console.log('в недели только 7 дней')
+// }
+
+// // switch в виде if
+
+// if(+num === 1){
+//     console.log('пн')
+// } else if(+num === 2){
+//     console.log('вт')
+// } else if(+num === 3){
+//     console.log('ср')
+// } else {
+//     console.log('что то нетак')
+// }
+
+// 2 < 1 
+//     ? console.log('no')
+//     : 3 > 2
+//     ? console.log('yes')
+//     : console.log('это не правда')
+
+
+// && - логическое и
+// || - логическое или
+// ! - логическое не
+
+
+// let num = +prompt()
+
+// if (num >= 0 && num <= 14){
+//     console.log('в первую четверть')
+// } else if(num >= 15 && num <=30){
+//     console.log('во вторую четьверть')
+// } else if(num >= 31 && num <= 45){
+//     console.log('третья четьверть')
+// } else if(num >= 46 && num <= 59){
+//     console.log('вчетвертую четьверт')
+// } else {
+//     console.log('ошибка')
+// }
+
+
+// let num = +prompt()
+
+// switch (num){
+//         case num = 0:
+//             case num = 1:  
+//                 case num = 2:
+//                     case num = 3:    
+//             alert('Зима')
+//             break
+//     case num = 4:
+//     case num = 5:
+//     case num = 6:  
+//         alert('Весна')
+//         break
+//     case num = 7:
+//     case num = 8:
+//     case num = 9:   
+//         alert('Лето')
+//         break
+//     case num = 10:
+//     case num = 11:
+//      case num = 12:   
+//         alert('Осень')
+//         break        
+//     }
+
+
+
+
+
+
+
+
+// циклы
+
+// let num = 'albert'
+
+// for(let i = 0; i < num.length ; i++ ){
+//     console.log(num[i])
+// }
+
+// let i = 0
+// while(i < num.length){
+//     console.log(num[i])
+//     i++
+// }
+
+// let num = prompt('you name')
+
+// for(i = 0; i < num.length; i++){
+//     alert(num.length)
+//     break
+// }
+
+// let i = 10
+// do{
+//     console.log('what')
+// }while (i < 6)            // выполняется один раз цикл, и потом проверяет
+
+
+// массивы
+
+// let arr = [1,2,3,4,5,6,7]
+
+// let arr2 = ['albert', 'zagrutdinov', 'firdusovih', 25, 'ear']
+
+// arr2.forEach((elem) => {
+//     console.log(elem)
+// })
+
+
+
+
+
+// шаблонные строки
+
+// let arr = [1,3,5,7,6]
+
+// console.log(`его зовут ${arr[4]}`)
+
+
+// сортировка
+
+// let arr = ['fkm,thn', 'nhbjr', 'otirto', 12]
+
+// console.log(arr.sort())
+
+
+
+
+// обьекты 
+
+// const firtsObj = {
+//     name: 'Albert',
+//     age: 29,
+//     child: 1,
+//     classes: ['Work', 'studies'],
+//     massiv: [
+//         {name: 'jfhd', age: 30},
+//         {name: 'sdrrg', age: 25}
+//     ]
+
+// }
+// //  console.log(firtsObj.name)
+// //  console.log(firtsObj.age)
+// //  console.log(firtsObj.child)
+// //  console.log(firtsObj.classes)
+
+//  firtsObj.secondName = 'Ivan'
+//  firtsObj.massiv[1].age = 45
+//  firtsObj.massiv.push({name: 'anna', age: 0})
+
+//  console.log(firtsObj)
+
+//  for(key in firtsObj){
+//      console.log(firtsObj[key])
+//  }
+
+//  let friends = {
+//      massiv: [
+//          {name: 'ivan', age: 25},
+//          {name: 'igor', age: 30},
+//          {name: 'oleg', age: 28},
+//          {name: 'eleonora', age: 20}
+//      ]
+//  }
+
+
+//  for(key in friends){
+//     console.log(friends.massiv)
+//  }
+
+
+// добавить => открывается втрой промпт и вводим значения
+// удалить через пробел индекс элемента
+// после каждого действия отображается в косоль лог
+
+
+
+let data = []
+
+
+const checkAction= (act) => {
+    if(act.toLowerCase() === 'добавить'){
+        return 1
+    } else if(act.toLowerCase() === 'удалить'){
+        return 0
+    }
+}
+
+const actionReduce = (act) => {
+    let newAction = prompt('продолжай')
+    if(act === 1){
+        addData(newAction)
+    } else {
+        delData(newAction)
+    }
+}
+
+const addData = (str) => {
+    data.push(str)
+}
+
+const delData = (num) => {
+    data.splice(num-1, 1)
+}
+
+
+while(true){
+    let action = prompt('введите действие')
+    actionReduce(checkAction(action))
+    console.log(data)
+}
